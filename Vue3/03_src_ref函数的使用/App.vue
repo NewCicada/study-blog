8fc45dfdfd6656344dc9663edd6ca8c9fc31cd2d
NewCicada <1547598968@qq.com>
2023-03-25 00:04:26 +0800

    chore:框架笔记

---
+++ Vue3/03_src_ref函数的使用/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <h1>一个人的信息</h1>
+  <h2>姓名:{{ name }}</h2>
+  <h2>年龄:{{ age }}</h2>
+  <h3>工作种类:{{ job.type }}</h3>
+  <h3>薪资:{{ job.salary }}</h3>
+  <button @click="changeInfo">修改人的信息</button>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  name: 'App',
+  setup() {
+    // 数据
+    let name = ref('张三')
+    let age = ref(18)
+    let job = ref({
+      type: '前端工程师',
+      salary: '30k',
+    })
+    // 方法
+    function changeInfo() {
+      name.value = '李四'
+      age.value = 48
+      // console.log(job.value) 
+      job.value.type = 'UI设计师'
+      job.value.salary = '80k'
+      // console.log(name, age)
+    }
+    // 返回一个对象
+    return {
+      name,
+      age,
+      changeInfo,
+      job,
+    }
+  },
+}
+</script>
+
+<style>
+</style>
